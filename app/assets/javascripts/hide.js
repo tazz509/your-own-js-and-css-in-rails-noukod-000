eacfe23437b1ba8323a53744fdbad1351846f396
carly <carly@noukod.com>
2019-03-27 18:07:14 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,1 +1,9 @@
-hideWhenClicked()
+function hideWhenClicked(){
+    const element = document.getElementById("hide_this")
+
+    if (element.style.display === "none") {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+}
