454e81532bc938efe504652f492e8a18f2562cf0
carly <carly@noukod.com>
2019-03-27 18:10:41 +0000

    Automatically backed up by Learn

--- spec/features/hide_link_spec.rb
+++ spec/features/hide_link_spec.rb
@@ -1,9 +1,9 @@
-# require 'rails_helper'
-#
-# feature "Hide Link", :js => true do
-#   scenario "Clicking on 'Click To Hide Me' hides itself" do
-#     visit '/'
-#     click_link 'Click To Hide Me'
-#     expect(page).not_to have_content 'Click To Hide Me'
-#   end
-# end
+require 'rails_helper'
+
+feature "Hide Link", :js => true do
+  scenario "Clicking on 'Click To Hide Me' hides itself" do
+    visit '/'
+    click_link 'Click To Hide Me'
+    expect(page).not_to have_content 'Click To Hide Me'
+  end
+end
